80f407add5172dd8d2f72e977c0b7339250fcc95
Evgeniy <bazhin_ea@taximaxim.ru>
2026-01-03 14:44:45 +0500

    Auto-generated commit

--- chunk-FIO4KPKM.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:()=>import("./chunk-CCNT7V7Q.js").then(t=>t.LayoutComponent),children:[{path:"",redirectTo:"main",pathMatch:"full"},{path:"main",loadComponent:()=>import("./chunk-NI6SY76N.js").then(t=>t.MainComponent)},{path:"**",redirectTo:"main"}]}];export{o as featuresLayoutRoutes};
