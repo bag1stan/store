7b5b00cd1247fcbff0cd8f8132e981c08d525588
Evgeniy <bazhin_ea@taximaxim.ru>
2026-01-05 00:52:48 +0500

    Auto-generated commit

--- chunk-AIGIPNTS.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var o=[{path:"",loadComponent:()=>import("./chunk-R2LPC5TV.js").then(t=>t.LayoutComponent),children:[{path:"",redirectTo:"main",pathMatch:"full"},{path:"main",loadComponent:()=>import("./chunk-LLFLXE3W.js").then(t=>t.MainComponent)},{path:"**",redirectTo:"main"}]}];export{o as featuresLayoutRoutes};
