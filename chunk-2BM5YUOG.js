6dd21febaa23235a3645c44eadb3b6cbc5cda3f7
Evgeniy <bazhin_ea@taximaxim.ru>
2026-01-05 19:39:47 +0500

    Auto-generated commit

--- chunk-2BM5YUOG.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var o=[{path:"",loadComponent:()=>import("./chunk-P7G75734.js").then(t=>t.LayoutComponent),children:[{path:"",redirectTo:"main",pathMatch:"full"},{path:"main",loadComponent:()=>import("./chunk-P5DHJGRS.js").then(t=>t.MainComponent)},{path:"**",redirectTo:"main"}]}];export{o as featuresLayoutRoutes};
