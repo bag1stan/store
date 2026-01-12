517fc8adb503f8ac9f37ee495a6f8ad6ff27d8b4
Evgeniy <bazhin_ea@taximaxim.ru>
2026-01-12 23:24:48 +0500

    feat: Minor upgrades

--- src/app/components/add-one-dialog/add-one-dialog.component.ts
+++ src/app/components/add-one-dialog/add-one-dialog.component.ts
@@ -12,6 +12,8 @@ import { TuiInputNumber } from '@taiga-ui/kit';
 import { injectContext } from '@taiga-ui/polymorpheus';
 import { kztSvg } from '../../icons/kzt.constant';
 import { Product } from '../../interfaces/product.interface';
+import { TuiInputYearModule, TuiSelectModule } from '@taiga-ui/legacy';
+import { StateService } from '../../services/state.service';
 
 
 @Component({
@@ -22,6 +24,9 @@ import { Product } from '../../interfaces/product.interface';
     ReactiveFormsModule,
     TuiInputNumber,
     TuiButton,
+    TuiInputYearModule,
+    TuiSelectModule,
+
   ],
   templateUrl: './add-one-dialog.component.html',
   styleUrl: './add-one-dialog.component.scss'
@@ -30,6 +35,8 @@ export class AddOneDialogComponent {
   private readonly fb =  inject(FormBuilder);
   private readonly alertService = inject(TuiAlertService);
   private readonly context = injectContext<TuiDialogContext<unknown, Product | void>>();
+
+  private readonly stateService = inject(StateService);
 
   readonly data = this.context.data;
   readonly isEditMode = Boolean(this.data);
@@ -45,6 +52,9 @@ export class AddOneDialogComponent {
     sold: this.fb.control<number>(0, Validators.required),
   })
 
+  readonly myCostRubControl = this.fb.control<number | null>(null);
+  readonly competitorsKztControl = this.fb.control<number | null>(null);
+
   constructor() {
     this.initFormValues()
 
@@ -53,11 +63,52 @@ export class AddOneDialogComponent {
     }
   }
 
+  onRubMyCostInput() {
+    setTimeout(() => {
+      const rub = this.myCostRubControl.value;
+      const kztCurrency = this.stateService.kztCurrency();
+
+      this.form.controls.my_cost.patchValue(rub ? Math.round(rub * kztCurrency) : null);
+    }, 0)
+  }
+
+  onKztMyCostInput() {
+    setTimeout(() => {
+      const kzt = this.form.controls.my_cost.value;
+      const kztCurrency = this.stateService.kztCurrency();
+
+      this.myCostRubControl.patchValue(kzt ? Math.round(kzt / kztCurrency) : null)
+    }, 0)
+  }
+
+  onRubCompetitorsInput() {
+    setTimeout(() => {
+      const rub = this.form.controls.competitors_price.value;
+      const kztCurrency = this.stateService.kztCurrency();
+
+      this.competitorsKztControl.patchValue(rub ? Math.round(rub * kztCurrency) : null);
+    }, 0)
+  }
+
+  onKztCompetitorsInput() {
+    setTimeout(() => {
+      const kzt = this.competitorsKztControl.value;
+      const kztCurrency = this.stateService.kztCurrency();
+
+      this.form.controls.competitors_price.patchValue(
+        kzt ? Math.round(kzt / kztCurrency) : null
+      );
+    }, 0)
+  }
+
   private initFormValues() {
     const data = this.data;
 
     if (data) {
       this.form.patchValue(data)
+
+      this.myCostRubControl.patchValue(Math.round(data.my_cost / this.stateService.kztCurrency()));
+      this.competitorsKztControl.patchValue(Math.round(data.competitors_price * this.stateService.kztCurrency()));
     }
   }
 
